7a3b5c97f740a8742bbbe96f7c2c8855700a5d08
dalofulgence <dal19006@byui.edu>
2021-10-22 12:18:16 -0600

    part1

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.static(__dirname));
+// 
+window.addEventListener('load', ()=>{
+    console.log('here is the page'); // Testing if i am getting any response
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    const xhttp = new XMLHttpRequest(); //  variable for the request
+    xhttp.onload = function() {
+
+        let text = JSON.parse(this.responseText); // get data from json to js object
+
+        let table = "<table class='table'>" + "<tr>" + "<th>" + "name" + "</th>" + "<th>" + "Website" + "</th>" +
+        "<th>" + "Email" + "</th>" + "<th>" + "Address" + "</th>" + "</tr>" ; // create table
+
+        // looping through the data array to dysplay info
+        for (let i = 0; i < text.length; i++){
+            table += 
+            "<tr id='pup'>" +
+             "<td  class='name'>" + text[i].name + "</td>" +
+             "<td>" + text[i].website + "</td>" +
+             "<td>" + text[i].email + "</td>" +
+             "<td>" + text[i].address.street + "," + text[i].address.city + " " + text[i].address.zipcode + "</td>" + "</tr>";
+        }
+        table += "</table>"; 
+        document.getElementById("info").innerHTML = table; // insert table into this div
+  
+    }
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/users "); // site to get data from
+    xhttp.send(); 
+    
+
 })
 
-app.listen(port, () => console.log('listening ' + port))
